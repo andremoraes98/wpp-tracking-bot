01ec907fa0d27afc8b50cead713691088241c408
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-05 09:10:38 -0300

    feat: implementação da função getTrackingData

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const axios = require('axios').default;
+const axios = require('axios');
 
-const codePackage = 'NL171221802BR';
-const url = `https://api.linketrack.com/track/json?user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo=${codePackage}`;
-
-axios.get(url)
-  .then(({ data: { eventos, codigo }}) => {
-    console.log(eventos, codigo);
-  })
-  .catch((error) => {
-    console.log(error.message);
-  })
+const getTrackingData = async (codePackage) => {
+  try {
+    const url = `https://api.linketrack.com/track/json?user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo=${codePackage}`;
+    const { data: { eventos, codigo }} = await axios.get(url)
+  
+    return { eventos, codigo };
+  } catch ({ response: { status, data } }) {
+    console.log(`Código: ${status}. ${data}`)
+  }
+}
